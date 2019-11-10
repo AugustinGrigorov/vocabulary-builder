4596242c044799e586a7666fca4b726512fe335c
Augustin Grigorov <a.a.grigorov@gmail.com>
2019-11-10 22:04:54 +0000

    Restore deployment script

--- src/customComponents/EditorCard/EditorCard.js
+++ src/customComponents/EditorCard/EditorCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import styled from 'styled-components/macro';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
