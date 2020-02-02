fd223f4d0d32c8823316223ff4d303241fb923ef
Augustin Grigorov <a.a.grigorov@gmail.com>
2020-02-02 12:28:20 +0000

    Visually complete

--- src/customComponents/FeedbackWidget/FeedbackWidget.js
+++ src/customComponents/FeedbackWidget/FeedbackWidget.js
@@ -1,25 +1,116 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components/macro';
-import feedbackIcon from './feedback.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { buttonBaseStyle } from '../../genericComponents/styles';
 
-const FeedbackIcon = styled.div`
-  display: flex;
+
+const Wrapper = styled.div`
   position: fixed;
-  padding: 12px;
-  background-color: #388E3C;
-  width: 32px;
-  box-shadow: 0 2px 4px rgba(0,0,0,0.4);
-  border-radius: 28px;
-  bottom: 12px;
-  right: 12px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  bottom: 24px;
+  right: 24px;
 `;
 
-function FeedbackWidget() {
-  return (
-    <FeedbackIcon>
-      <img alt="Give feedback" src={feedbackIcon} />
-    </FeedbackIcon>
-  );
+const CloseIcon = styled(FontAwesomeIcon)`
+  background: #fff;
+  font-size: 24px;
+  border-radius: 12px;
+  position: absolute;
+  top: -18px;
+  right: -12px;
+`;
+
+const FeedbackForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  background-color: #388E3C;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.4);
+  padding: 8px;
+  margin-bottom: 8px;
+  border-radius: 4px;
+`;
+
+const FeedbackField = styled.textarea`
+  resize: none;
+  width: 300px;
+  height: 120px;
+  margin-bottom: 4px;
+  font-size: 16px;
+`;
+
+const SubmitButton = styled.input`
+  ${buttonBaseStyle}
+  background: #1B5E20;
+  margin: 0 auto;
+`;
+
+const IconContainer = styled.div`
+  padding: 12px;
+  box-sizing: border-box;
+  background-color: #388E3C;
+  width: 52px;
+  height: 52px;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.4);
+  border-radius: 28px;
+  color: #fff;
+  font-size: 28px;
+`;
+
+class FeedbackWidget extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      feedback: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    const { feedback } = this.state;
+    console.log(`Feedback submitted: ${feedback}`);
+    e.preventDefault();
+  }
+
+  handleChange(e) {
+    this.setState({ feedback: e.target.value });
+  }
+
+  toggle(state) {
+    this.setState({
+      isOpen: state,
+    });
+  }
+
+  render() {
+    const { isOpen, feedback } = this.state;
+
+    return (
+      <Wrapper>
+        {isOpen ? (
+          <>
+            <CloseIcon icon="times-circle" onClick={() => this.toggle(false)} />
+            <FeedbackForm onSubmit={this.handleSubmit}>
+              <FeedbackField
+                id="feedback"
+                name="feedback"
+                value={feedback}
+                onChange={this.handleChange}
+              />
+              <SubmitButton type="submit" value="Submit Feedback" />
+            </FeedbackForm>
+          </>
+        ) : null}
+        <IconContainer onClick={() => this.toggle(true)}>
+          <FontAwesomeIcon icon="comment-alt" />
+        </IconContainer>
+      </Wrapper>
+    );
+  }
 }
 
 export default FeedbackWidget;
---
+++ src/genericComponents/styles.js
@@ -0,0 +1,14 @@
+import { css } from 'styled-components/macro';
+
+export const buttonBaseStyle = css`
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  padding: 8px 16px;
+  border: none;
+  text-align: center;
+  border-radius: 18px;
+  text-transform: uppercase;
+  margin: 12px auto;
+  color: #FFF;
+`;
