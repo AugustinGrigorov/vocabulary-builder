ca098b6671686afac1286ae4ae8055cf718fd316
Augustin Grigorov <a.a.grigorov@gmail.com>
2020-02-28 23:49:22 +0000

    First test suite

--- cypress/integration/learn_page_spec.js
+++ cypress/integration/learn_page_spec.js
@@ -1,6 +1,9 @@
 const WORD = 'test word';
 const DEFINITION = 'test definition';
 const EXAMPLE = 'test example';
+const UPDATED_WORLD = 'updated test word';
+const UPDATED_DEFINITION = 'updated test definition';
+const UPDATED_EXAMPLE = 'updated test example';
 
 describe('Learn page', () => {
   it('successfully loads', () => {
@@ -9,9 +12,9 @@ describe('Learn page', () => {
 
   it('creates a word correcly', () => {
     cy.get('div[data-cy="editor"]').click();
-    cy.get('input[id="word"]').type(WORD, { force: true });
-    cy.get('input[id="definition"]').type(DEFINITION, { force: true });
-    cy.get('input[id="example"]').type(EXAMPLE, { force: true });
+    cy.get('#word').type(WORD, { force: true });
+    cy.get('#definition').type(DEFINITION, { force: true });
+    cy.get('#example').type(EXAMPLE, { force: true });
     cy.get('div[data-cy="editor"] input[type="submit"]').click({ force: true });
     cy.get('div[class^="Card__Container"]:nth-child(2) h2').should('contain', WORD);
     cy.get('div[class^="Card__Container"]:not([data-cy="pending"]):not([data-cy="editor"])').then(() => {
@@ -20,5 +23,23 @@ describe('Learn page', () => {
       cy.get('div[class^="Card__Container"]:nth-child(2) p[class^="WordCard__Definition"]').should('contain', DEFINITION);
       cy.get('div[class^="Card__Container"]:nth-child(2) p[class^="WordCard__Example"]').should('contain', EXAMPLE);
     });
+  });
+  it('edits a word successfully', () => {
+    cy.get('svg[class*="WordCard__EditButton"]').click({ force: true });
+    cy.get('div[class^="Card__Container"]:nth-child(2) #word').clear({ force: true }).type(UPDATED_WORLD, { force: true });
+    cy.get('div[class^="Card__Container"]:nth-child(2) #definition').clear({ force: true }).type(UPDATED_DEFINITION, { force: true });
+    cy.get('div[class^="Card__Container"]:nth-child(2) #example').clear({ force: true }).type(UPDATED_EXAMPLE, { force: true });
+    cy.get('div[class^="Card__Container"]:nth-child(2) input[class^="WordForm__Submit"]').click({ force: true });
+    cy.get('div[class^="Card__Container"]:not([data-cy="pending"]):not([data-cy="editor"])').then(() => {
+      cy.get('div[class^="Card__Container"]:nth-child(2)').click();
+      cy.get('div[class^="Card__Container"]:nth-child(2) h2[class^="WordCard__Word"]').should('contain', UPDATED_WORLD);
+      cy.get('div[class^="Card__Container"]:nth-child(2) p[class^="WordCard__Definition"]').should('contain', UPDATED_DEFINITION);
+      cy.get('div[class^="Card__Container"]:nth-child(2) p[class^="WordCard__Example"]').should('contain', UPDATED_EXAMPLE);
+    });
+  });
+  it('deletes a word successfully', () => {
+    cy.get('div[class^="Card__Container"]:nth-child(2)').click();
+    cy.get('svg[class*="WordCard__RemoveButton"]').click({ force: true });
+    cy.get('div[class^="Card__Container"]:nth-child(2)').should('not.exist');
   });
 });
